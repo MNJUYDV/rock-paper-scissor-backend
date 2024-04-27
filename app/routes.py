27c62469b37f905ce011763bd099c86e1a1dd00b
Manju Yadav <manjuyadav@Manjus-iMac.home>
2024-04-27 19:06:17 -0400

    modified docker file

--- app/routes.py
+++ app/routes.py
@@ -3,13 +3,13 @@ from app.services.leaderboard_service import LeaderBoardService
 
 leaderboard_bp = Blueprint('leaderboard_bp', __name__)
 
-@leaderboard_bp.route('/leaderboard', methods=['GET'])
+@leaderboard_bp.route('/api/v1/leaderboard', methods=['GET'])
 def get_leaderboard():
     response_data = LeaderBoardService.get_leaderboard()
     return jsonify({"leaderboard_stats": response_data})
 
 
-@leaderboard_bp.route('/leaderboard', methods=['POST'])
+@leaderboard_bp.route('/api/v1/leaderboard', methods=['POST'])
 def create_leaderboard_entry():
     LeaderBoardService.create_leaderboard_entry(request)
     return jsonify({'message': 'Leaderboard entry created successfully'}), 201
