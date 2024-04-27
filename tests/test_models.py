3f7c6770681a17b7476e413c96d560beeee00746
Manju Yadav <manjuyadav@Manjus-iMac.home>
2024-04-27 17:44:08 -0400

    added service test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,6 @@
 import unittest
 from app.models.player import Player
 from app.models.game import Game
-from app.models.game_status import GameStatus
 
 class TestPlayerModel(unittest.TestCase):
     # Test cases for Player model
@@ -11,7 +10,7 @@ class TestGameModel(unittest.TestCase):
     # Test cases for Game model
     pass
 
-class TestGameStatusModel(unittest.TestCase):
+class TestLeaderBoardModel(unittest.TestCase):
     # Test cases for GameStatus model
     pass
 
